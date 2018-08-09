9585099ddc7a94d8ee45a87b073a7ffc164d1ca2
Mark Allen <dr.mark.allen@gmail.com>
2018-08-09 21:53:25 +0100

    Require logged-in user for all filemanger views

--- fileshare/filemanager/views.py
+++ fileshare/filemanager/views.py
@@ -1,19 +1,20 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
 
 from .models import FileUpload
 
 
-class FileUploadDetail(DetailView):
+class FileUploadDetail(LoginRequiredMixin, DetailView):
     model = FileUpload
 
 
-class FileUploadList(ListView):
+class FileUploadList(LoginRequiredMixin, ListView):
     model = FileUpload
     context_object_name = 'file_upload_list'
 
 
-class FileUploadCreate(CreateView):
+class FileUploadCreate(LoginRequiredMixin, CreateView):
     model = FileUpload
     fields = ['description', 'file', 'public']
 
