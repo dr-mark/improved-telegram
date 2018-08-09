7eb442e3a054b5e345c875796065d321c5c0d37d
Mark Allen <dr.mark.allen@gmail.com>
2018-08-09 20:41:10 +0100

    Initial model definition for file uploads

--- fileshare/filemanager/models.py
+++ fileshare/filemanager/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class FileUpload(models.Model):
+    """
+    This model represents a file upload
+    """
+    description = models.CharField(max_length=255, blank=True)
+    file = models.FileField(upload_to='documents/')
+    public = models.BooleanField()
+    created_by = models.ForeignKey(get_user_model())
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
+
+    def get_absolute_url(self):
+        return reverse('filemanager:detail', kwargs={'pk': self.pk})
