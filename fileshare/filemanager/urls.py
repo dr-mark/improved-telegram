ba41d79a0883070bf0788e367be0b1b90e33c564
Mark Allen <dr.mark.allen@gmail.com>
2018-08-09 20:58:14 +0100

    Add way to upload a file through the site front-end

--- fileshare/filemanager/urls.py
+++ fileshare/filemanager/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 
-from .views import FileUploadDetail, FileUploadList
+from .views import FileUploadDetail, FileUploadList, FileUploadCreate
 
 
 app_name = 'filemanager'
 urlpatterns = [
     url(r'^$', FileUploadList.as_view(), name='list'),
     url(r'^(?P<pk>[0-9]+)/$', FileUploadDetail.as_view(), name='detail'),
+    url(r'^add/$', FileUploadCreate.as_view(), name='create'),
 ]
--- fileshare/filemanager/views.py
+++ fileshare/filemanager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
 
 from .models import FileUpload
@@ -11,3 +12,12 @@ class FileUploadDetail(DetailView):
 class FileUploadList(ListView):
     model = FileUpload
     context_object_name = 'file_upload_list'
+
+
+class FileUploadCreate(CreateView):
+    model = FileUpload
+    fields = ['description', 'file', 'public']
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super(FileUploadCreate, self).form_valid(form)
