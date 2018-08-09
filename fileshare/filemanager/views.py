1ddf4df3d67458a2c500a5f1108c5e1b9caecc95
Mark Allen <dr.mark.allen@gmail.com>
2018-08-09 21:54:37 +0100

    Only show FileUpload detail if public or created by current user

--- fileshare/filemanager/views.py
+++ fileshare/filemanager/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
@@ -5,8 +6,26 @@ from django.views.generic import ListView, DetailView
 from .models import FileUpload
 
 
-class FileUploadDetail(LoginRequiredMixin, DetailView):
+class ViewPermissionsMixin(object):
+    """
+    Base class for all custom permission mixins to inherit from
+    """
+    def has_permissions(self):
+        # Can use self.get_object() and self.request.user here
+        return True
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.has_permissions():
+            raise PermissionDenied
+        return super(ViewPermissionsMixin, self).dispatch(
+            request, *args, **kwargs)
+
+
+class FileUploadDetail(LoginRequiredMixin, ViewPermissionsMixin, DetailView):
     model = FileUpload
+
+    def has_permissions(self):
+        return self.request.user == self.get_object().created_by or self.get_object().public
 
 
 class FileUploadList(LoginRequiredMixin, ListView):
